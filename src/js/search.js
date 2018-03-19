b3a90f70042ffe74ecee4fc969d725f4a1a8d6a3
Andrey Ivanov <andrey-tm203@mail.ru>
2018-03-19 12:07:22 +0300

    some correctings

--- src/js/search.js
+++ src/js/search.js
@@ -1,21 +1,44 @@
 const searchContainer = document.querySelector('.user__search');
 const nav = document.querySelector('.main-nav');
-const button = searchContainer.querySelector('.user__search-toggle');
+const toggle = searchContainer.querySelector('.user__search-toggle');
 const field = searchContainer.querySelector('.user__search-field');
+const button = searchContainer.querySelector('.user__search-btn');
 const explore = nav.querySelector('.main-nav__item--explore');
 const categories = nav.querySelector('.main-nav__item--categories');
 
+const activateSearch = () => {
+  searchContainer.classList.add('user__search--active');
+  explore.hidden = true;
+  categories.hidden = true;
+};
+const deactivateSearch = () => {
+  searchContainer.classList.remove('user__search--active');
+  explore.hidden = false;
+  categories.hidden = false;
+};
+
+const activateButton = () => {
+  if (!button.classList.contains('user__search-btn--active')) {
+    button.classList.add('user__search-btn--active');
+    button.disabled = false;
+  }
+};
+const deactivateButton = () => {
+  if (button.classList.contains('user__search-btn--active')) {
+    button.classList.remove('user__search-btn--active');
+    button.disabled = true;
+  }
+};
+
 const initSearch = () => {
-  button.addEventListener('click', evt => {
-    if (!searchContainer.classList.contains('user__search--active')) {
-      searchContainer.classList.add('user__search--active');
-      explore.hidden = true;
-      categories.hidden = true;
-    } else {
-      searchContainer.classList.remove('user__search--active');
-      explore.hidden = false;
-      categories.hidden = false;
-    }
+  toggle.addEventListener('click', evt => {
+    !searchContainer.classList.contains('user__search--active')
+      ? activateSearch()
+      : deactivateSearch();
+  });
+
+  field.addEventListener('input', evt => {
+    evt.target.value ? activateButton() : deactivateButton();
   });
 };
 
